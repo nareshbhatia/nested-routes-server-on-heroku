5cd4280a912e5e3d6c3c1e8797f7d79ce390bbd5
Naresh Bhatia <naresh.archfirst@gmail.com>
2021-12-14 00:31:38 -0500

    Initial commit

---
+++ src/routes/models.ts
@@ -0,0 +1,30 @@
+export interface Account {
+  id: string;
+  name: string;
+}
+export interface Security {
+  id: string;
+  name: string;
+  price: number;
+  industryId: string;
+}
+
+export interface Holding {
+  id: string;
+  symbol: string;
+  quantity: number;
+  accountId: string;
+}
+
+export interface Order {
+  id: string;
+  side: string;
+  symbol: string;
+  quantity: number;
+  type: string;
+  limitPrice: number | null;
+  status: string;
+  accountId: string;
+  createdAt: string;
+  createdBy: string;
+}
